cc86a8baa526ee704b2288b45a67dd1c11168884
Partha Ghosh <partha.ghosh@alfasystems.com>
2021-08-13 16:43:57 +0100

    Remove unused import

--- morf-testsupport/src/main/java/org/alfasoftware/morf/jdbc/AbstractSqlDialectTest.java
+++ morf-testsupport/src/main/java/org/alfasoftware/morf/jdbc/AbstractSqlDialectTest.java
@@ -112,7 +112,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Base64;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
