a343420079c218f090c247c45f01da59e2543f24
Marcello Romano <marcello.romano@alfasystems.com>
2021-09-01 10:30:44 +0100

    Fixing Javadoc and formatting.

--- morf-core/src/main/java/org/alfasoftware/morf/sql/DirectPathQueryHint.java
+++ morf-core/src/main/java/org/alfasoftware/morf/sql/DirectPathQueryHint.java
@@ -1,17 +1,15 @@
 package org.alfasoftware.morf.sql;
 
 /**
- * Singleton class Representing the hint for a direct path query.
+ * Singleton class representing the hint for a direct path query.
  *
  * @author Copyright (c) Alfa Financial Software Limited. 2021
  */
 public final class DirectPathQueryHint implements Hint {
 
-
   private DirectPathQueryHint() {
     super();
   }
-
 
   private static DirectPathQueryHint INSTANCE = new DirectPathQueryHint();
 
