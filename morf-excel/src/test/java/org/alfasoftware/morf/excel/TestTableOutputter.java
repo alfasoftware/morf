e3e6794903223eda94fb040b70f8670af49c49ed
Graham Hadden <graham.hadden@alfasystems.com>
2023-05-24 17:01:08 +0100

    Fix further code smells in TableOutputter and add test coverage

---
+++ morf-excel/src/test/java/org/alfasoftware/morf/excel/TestTableOutputter.java
@@ -0,0 +1,121 @@
+package org.alfasoftware.morf.excel;
+
+import static org.alfasoftware.morf.metadata.SchemaUtils.column;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.alfasoftware.morf.dataset.Record;
+import org.alfasoftware.morf.metadata.DataType;
+import org.alfasoftware.morf.metadata.Table;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import com.google.common.collect.ImmutableList;
+import jxl.Cell;
+import jxl.write.WritableCell;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+
+/**
+ * Ensure that {@link TableOutputter} works correctly.
+ *
+ * @author Copyright (c) Alfa Financial Software 2023
+ */
+public class TestTableOutputter {
+
+  private Table table;
+  private TableOutputter tableOutputter;
+  private WritableSheet writableSheet;
+  private WritableWorkbook writableWorkbook;
+
+
+  /**
+   * Set up the test.
+   */
+  @Before
+  public void setUp() {
+    table = mock(Table.class);
+    when(table.getName()).thenReturn("TestTable");
+    when(table.columns()).thenReturn(ImmutableList.of(
+        column("Col1", DataType.STRING),
+        column("Col2", DataType.DECIMAL),
+        column("Col3", DataType.BIG_INTEGER),
+        column("Col4", DataType.INTEGER),
+        column("Col5", DataType.CLOB)
+    ));
+
+    writableWorkbook = mock(WritableWorkbook.class);
+    writableSheet = mock(WritableSheet.class);
+    when(writableSheet.getName()).thenReturn("Sheet1");
+    when(writableSheet.getWritableCell(anyInt(), anyInt())).thenReturn(mock(WritableCell.class));
+    when(writableSheet.getCell(anyInt(), anyInt())).thenReturn(mock(Cell.class));
+    when(writableWorkbook.createSheet(any(String.class), any(Integer.class))).thenReturn(writableSheet);
+
+    AdditionalSchemaData additionalSchemaData = mock(AdditionalSchemaData.class);
+    tableOutputter = new TableOutputter(additionalSchemaData);
+  }
+
+
+  /**
+   * Tests that cells with different data types are written correctly to the worksheet.
+   */
+  @Test
+  public void testTable() throws WriteException {
+    // Given
+    Record record1 = mock(Record.class);
+    when(record1.getString("Col1")).thenReturn("STRING VALUE");
+    when(record1.getBigDecimal("Col2")).thenReturn(new BigDecimal(12.34));
+    when(record1.getLong("Col3")).thenReturn(22L);
+    when(record1.getLong("Col4")).thenReturn(33L);
+    when(record1.getString("Col5")).thenReturn("CLOB VALUE");
+    ImmutableList<Record> recordList = ImmutableList.<Record>of(record1);
+
+    // When
+    tableOutputter.table(1, writableWorkbook, table, recordList);
+
+    ArgumentCaptor<WritableCell> writableCellCaptor = ArgumentCaptor.forClass(WritableCell.class);
+    verify(writableSheet, times(36)).addCell(writableCellCaptor.capture());
+    List<WritableCell> capturedWritableCellList = writableCellCaptor.getAllValues();
+    // Example Data
+    assertTrue(isCapturedWritableCellListContains(capturedWritableCellList,12, 0, "STRING VALUE"));
+    assertTrue(isCapturedWritableCellListContains(capturedWritableCellList,12, 1, BigDecimal.valueOf(12.34).toString()));
+    assertTrue(isCapturedWritableCellListContains(capturedWritableCellList,12, 2, Long.valueOf(22).toString()));
+    assertTrue(isCapturedWritableCellListContains(capturedWritableCellList,12, 3, Long.valueOf(33).toString()));
+    assertTrue(isCapturedWritableCellListContains(capturedWritableCellList,12, 4, "CLOB VALUE"));
+
+    // Parameters to Set Up
+    assertTrue(isCapturedWritableCellListContains(capturedWritableCellList,16, 0, "STRING VALUE"));
+    assertTrue(isCapturedWritableCellListContains(capturedWritableCellList,16, 1, BigDecimal.valueOf(12.34).toString()));
+    assertTrue(isCapturedWritableCellListContains(capturedWritableCellList,16, 2, Long.valueOf(22).toString()));
+    assertTrue(isCapturedWritableCellListContains(capturedWritableCellList,16, 3, Long.valueOf(33).toString()));
+    assertTrue(isCapturedWritableCellListContains(capturedWritableCellList,16, 4, "CLOB VALUE"));
+  }
+
+
+  /**
+   * Tests whether a given {@link WritableCell} {@link List} contains an entry with a given row, column and value.
+   *
+   * @param capturedWritableCellList the {@link WritableCell} {@link List}
+   * @param row the row
+   * @param col the column
+   * @param value the value
+   * @return true if capturedWritableCellList contains the entry, otherwise false
+   */
+  private boolean isCapturedWritableCellListContains(List<WritableCell> capturedWritableCellList, int row, int col, String value) {
+    return capturedWritableCellList.stream()
+      .filter(writableCell -> writableCell.getRow() == row &&
+          writableCell.getColumn() == col &&
+          writableCell.getContents().equals(value))
+      .count() == 1;
+  }
+}
