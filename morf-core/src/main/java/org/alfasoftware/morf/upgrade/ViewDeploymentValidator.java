b6ebf82910b3d590c13e92fcb174d78a29252fe3
Michael Parkin <michael.parkin@alfasystems.com>
2023-06-22 11:45:22 +0100

    change methods to default to ensure backwards compatibility

--- morf-core/src/main/java/org/alfasoftware/morf/upgrade/ViewDeploymentValidator.java
+++ morf-core/src/main/java/org/alfasoftware/morf/upgrade/ViewDeploymentValidator.java
@@ -19,10 +19,45 @@ public interface ViewDeploymentValidator {
    * first need to be de-registered, to clear any potential remnants, and only then the new view will be created.
    *
    * @param view View being examined. This is an existing schema view.
+   * @param upgradeSchemas Source and target schemas used for upgrade.
    * @return This method should return true, if the view is validated and does not need updating.
    *         This method can return false to trigger full view re-definition.
    */
-  public boolean validateExistingView(View view);
+  default boolean validateExistingView(View view, UpgradeSchemas upgradeSchemas){
+    return validateExistingView(view);
+  }
+
+  /**
+   * Called during {@link ExistingViewStateLoader#viewChanges(org.alfasoftware.morf.metadata.Schema, org.alfasoftware.morf.metadata.Schema)},
+   * for each view that exists and appears to be valid. Validators should return true to further validate the view from their point of view.
+   * If fully validated, the view will be left in it's current state, otherwise, if a validator responds with false, any old view records will
+   * first need to be de-registered, to clear any potential remnants, and only then the new view will be created.
+   *
+   * @param view View being examined. This is an existing schema view.
+   * @return This method should return true, if the view is validated and does not need updating.
+   *         This method can return false to trigger full view re-definition.
+   * @deprecated kept to ensure backwards compatibility.
+   */
+  @Deprecated
+  default boolean validateExistingView(View view){
+    throw new IllegalStateException();
+  }
+
+
+  /**
+   * Called during {@link ExistingViewStateLoader#viewChanges(org.alfasoftware.morf.metadata.Schema, org.alfasoftware.morf.metadata.Schema)},
+   * for each view that does not yet exist in the database, and is to be created. Validators should return true to acknowledge they also do not
+   * yet recognise the view. If acknowledged, the view will be created, otherwise, if a validator responds with false, any old view records will
+   * first need to be de-registered, to clear any potential remnants, and only then the new view will be created.
+   *
+   * @param view View being examined. This is a non-existing schema view.
+   * @param upgradeSchemas Source and target schemas used for upgrade.
+   * @return This method should return true, if the view can simply be created.
+   *         This method can return false to trigger full view re-definition.
+   */
+  default boolean validateMissingView(View view, UpgradeSchemas upgradeSchemas){
+    return validateExistingView(view);
+  }
 
   /**
    * Called during {@link ExistingViewStateLoader#viewChanges(org.alfasoftware.morf.metadata.Schema, org.alfasoftware.morf.metadata.Schema)},
@@ -33,8 +68,12 @@ public interface ViewDeploymentValidator {
    * @param view View being examined. This is a non-existing schema view.
    * @return This method should return true, if the view can simply be created.
    *         This method can return false to trigger full view re-definition.
+   * @deprecated kept to ensure backwards compatibility.
    */
-  public boolean validateMissingView(View view);
+  @Deprecated
+  default boolean validateMissingView(View view){
+    throw new IllegalStateException();
+  }
 
 
   /**
@@ -43,11 +82,23 @@ public interface ViewDeploymentValidator {
   class AlwaysValidate implements ViewDeploymentValidator {
 
     @Override
+    public boolean validateExistingView(View view, UpgradeSchemas upgradeSchemas) {
+      return true; // the given existing view is okay as is
+    }
+
+    @Override
+    @Deprecated
     public boolean validateExistingView(View view) {
       return true; // the given existing view is okay as is
     }
 
     @Override
+    public boolean validateMissingView(View view, UpgradeSchemas upgradeSchemas) {
+      return true; // the given new view can be created right away
+    }
+
+    @Override
+    @Deprecated
     public boolean validateMissingView(View view) {
       return true; // the given new view can be created right away
     }
