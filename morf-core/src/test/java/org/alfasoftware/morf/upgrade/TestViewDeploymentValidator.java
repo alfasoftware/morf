a6443425568807e1b750facda6e6ba955a2be97d
Michael Parkin <michael.parkin@alfasystems.com>
2023-06-23 09:03:35 +0100

    up test coverage

--- morf-core/src/test/java/org/alfasoftware/morf/upgrade/TestViewDeploymentValidator.java
+++ morf-core/src/test/java/org/alfasoftware/morf/upgrade/TestViewDeploymentValidator.java
@@ -1,11 +1,14 @@
 package org.alfasoftware.morf.upgrade;
 
 import junit.framework.TestCase;
+import org.alfasoftware.morf.jdbc.ConnectionResources;
 import org.alfasoftware.morf.metadata.View;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
+import static org.alfasoftware.morf.metadata.SchemaUtils.schema;
 import static org.alfasoftware.morf.metadata.SchemaUtils.view;
 import static org.alfasoftware.morf.sql.SqlUtils.*;
 
@@ -13,21 +16,37 @@ public class TestViewDeploymentValidator {
 
     ViewDeploymentValidator viewDeploymentValidator;
     View testView;
+    UpgradeSchemas upgradeSchemas;
+
+    ViewDeploymentValidator.Factory.AlwaysValidateFactory alwaysValidateFactory = new ViewDeploymentValidator.Factory.AlwaysValidateFactory();
+
+    @Mock
+    ConnectionResources connectionResources;
 
     @Before
     public void setup() {
-        viewDeploymentValidator = new ViewDeploymentValidator.AlwaysValidate();
+        viewDeploymentValidator = alwaysValidateFactory.createViewDeploymentValidator(connectionResources);
+        upgradeSchemas = new UpgradeSchemas(schema(), schema());
         testView      = view("FooView", select(field("name")).from(tableRef("Foo")));
     }
 
     @Test
     public void testValidateExistingView() {
-        Assert.assertTrue(viewDeploymentValidator.validateExistingView(testView));
+        Assert.assertTrue(viewDeploymentValidator.validateExistingView(testView, upgradeSchemas));
     }
 
     @Test
     public void testValidateMissingView() {
-        Assert.assertTrue(viewDeploymentValidator.validateMissingView(testView));
+        Assert.assertTrue(viewDeploymentValidator.validateMissingView(testView, upgradeSchemas));
     }
 
+    @Test
+    public void testDeprecatedValidateExistingView() {
+        Assert.assertTrue(viewDeploymentValidator.validateExistingView(testView));
+    }
+
+    @Test
+    public void testDeprecatedValidateMissingView() {
+        Assert.assertTrue(viewDeploymentValidator.validateMissingView(testView));
+    }
 }
