6356c287a05e68694d45792bc1a58ed725b91f8b
Luke Riou <luke.riou@alfasystems.com>
2023-10-31 11:09:41 +0000

    Refactor the SQL parser to handle comments within SQL queries.

--- morf-core/src/test/java/org/alfasoftware/morf/jdbc/TestNamedParameterPreparedStatement.java
+++ morf-core/src/test/java/org/alfasoftware/morf/jdbc/TestNamedParameterPreparedStatement.java
@@ -15,10 +15,8 @@
 
 package org.alfasoftware.morf.jdbc;
 
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -26,6 +24,7 @@ import static org.mockito.Mockito.when;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.alfasoftware.morf.jdbc.NamedParameterPreparedStatement.ParseResult;
 import org.junit.Test;
@@ -33,7 +32,7 @@ import org.junit.Test;
 /**
  * Tests {@link NamedParameterPreparedStatement}.
  *
- * @author Copyright (c) Alfa Financial Software 2014
+ * @author Copyright (c) Alfa Financial Software 2023
  */
 public class TestNamedParameterPreparedStatement {
 
@@ -45,16 +44,60 @@ public class TestNamedParameterPreparedStatement {
   @Test
   public void testParse() {
     ParseResult parseResult = NamedParameterPreparedStatement.parseSql(
-      "SELECT :fee,:fi, :fo(:fum), ':eek' FROM :fum WHERE :fum AND :fo",
-      mock(SqlDialect.class)
+        "SELECT :fee,:fi, :fo(:fum), ':eek' FROM :fum WHERE :fum AND :fo",
+        mock(SqlDialect.class)
     );
 
-    assertThat("Parsed SQL", parseResult.getParsedSql(),                equalTo("SELECT ?,?, ?(?), ':eek' FROM ? WHERE ? AND ?"));
-    assertThat("fee",        parseResult.getIndexesForParameter("fee"), contains(1));
-    assertThat("fi",         parseResult.getIndexesForParameter("fi"),  contains(2));
-    assertThat("fo",         parseResult.getIndexesForParameter("fo"),  contains(3, 7));
-    assertThat("fum",        parseResult.getIndexesForParameter("fum"), contains(4, 5, 6));
-    assertThat("eek",        parseResult.getIndexesForParameter("eek"), hasSize(0));
+    assertEquals("Parsed SQL",
+        "SELECT ?,?, ?(?), ':eek' FROM ? WHERE ? AND ?",
+        parseResult.getParsedSql()
+    );
+    assertTrue(parseResult.getIndexesForParameter("fee").contains(1));
+    assertTrue(parseResult.getIndexesForParameter("fi").contains(2));
+
+    List<Integer> foIndexes = parseResult.getIndexesForParameter("fo");
+    assertTrue(foIndexes.contains(3));
+    assertTrue(foIndexes.contains(7));
+
+    List<Integer> fumIndexes = parseResult.getIndexesForParameter("fum");
+    assertTrue(fumIndexes.contains(4));
+    assertTrue(fumIndexes.contains(5));
+    assertTrue(fumIndexes.contains(6));
+
+    assertTrue(parseResult.getIndexesForParameter("eek").isEmpty());
+  }
+
+
+  /**
+   * Ensures that the `indexMap` is properly populated with parameter information when the SQL query contains comments.
+   * The SQL query provided contains parameters, a comment and some text.
+   * The parsed SQL should have '?' characters in place of parameters and should ignore comments.
+   */
+  @Test
+  public void testParseWithComments() {
+    ParseResult parseResult = NamedParameterPreparedStatement.parseSql(
+        "SELECT :fee,:fi, :fo(:fum), ':eek' FROM :fum\n-- Comment :bang\nWHERE :fum AND :fo",
+        mock(SqlDialect.class)
+    );
+
+    assertEquals("Parsed SQL",
+        "SELECT ?,?, ?(?), ':eek' FROM ?\n-- Comment :bang\nWHERE ? AND ?",
+        parseResult.getParsedSql()
+    );
+    assertTrue(parseResult.getIndexesForParameter("fee").contains(1));
+    assertTrue(parseResult.getIndexesForParameter("fi").contains(2));
+
+    List<Integer> foIndexes = parseResult.getIndexesForParameter("fo");
+    assertTrue(foIndexes.contains(3));
+    assertTrue(foIndexes.contains(7));
+
+    List<Integer> fumIndexes = parseResult.getIndexesForParameter("fum");
+    assertTrue(fumIndexes.contains(4));
+    assertTrue(fumIndexes.contains(5));
+    assertTrue(fumIndexes.contains(6));
+
+    assertTrue(parseResult.getIndexesForParameter("bang").isEmpty());
+    assertTrue(parseResult.getIndexesForParameter("eek").isEmpty());
   }
 
 
