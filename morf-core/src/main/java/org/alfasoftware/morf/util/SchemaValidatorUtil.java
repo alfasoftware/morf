2799248adeb86a11830517aa0e1991ccd555fd77
Michael Parkin <michael.parkin@alfasystems.com>
2024-01-11 09:16:30 +0000

    fix code smells

--- morf-core/src/main/java/org/alfasoftware/morf/util/SchemaValidatorUtil.java
+++ morf-core/src/main/java/org/alfasoftware/morf/util/SchemaValidatorUtil.java
@@ -3,9 +3,11 @@ package org.alfasoftware.morf.util;
 /**
  * Util class to help validate Schema details
  */
-public class SchemaValidatorUtil {
+public final class SchemaValidatorUtil {
 
-    private static final String schemaNameValidationPattern = "^[A-Za-z0-9_]*$";
+    private static final String SCHEMA_NAME_VALIDATION_PATTERN = "^[A-Za-z0-9_]*$";
+
+    private SchemaValidatorUtil() {}
 
     /**
      * throws a IllegalArgumentException when schemaName contains illegal chars.
@@ -14,9 +16,9 @@ public class SchemaValidatorUtil {
      * @throws IllegalArgumentException - when the schemaName contains invalid chars.
      */
     public static String validateSchemaName(String schemaName) {
-        if(schemaName != null && !schemaName.matches(schemaNameValidationPattern)) {
+        if(schemaName != null && !schemaName.matches(SCHEMA_NAME_VALIDATION_PATTERN)) {
             throw new IllegalArgumentException("schemaName [" + schemaName + "] failed validation check.");
-        };
+        }
         return schemaName;
     }
 
