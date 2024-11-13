759405db4c9c211a11528563723f579c1dd542d6
jsimlo <jsimlo@users.noreply.github.com>
2024-11-13 19:01:41 +0000

    Adding validation preventing standard_conforming_strings=off

---
+++ morf-core/src/test/java/org/alfasoftware/morf/jdbc/TestAbstractConnectionResources.java
@@ -0,0 +1,56 @@
+package org.alfasoftware.morf.jdbc;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+public class TestAbstractConnectionResources {
+
+
+  @Test
+  public void testValidatePgConnectionStandardConformingStringsOther() {
+    AbstractConnectionResources.validatePgConnectionStandardConformingStrings("OTHER", mock(Connection.class));
+  }
+
+
+  @Test
+  public void testValidatePgConnectionStandardConformingStringsTrue() {
+    MockPgConnection mockPgConnection = mock(MockPgConnection.class);
+    when(mockPgConnection.getStandardConformingStrings()).thenReturn(true);
+
+    AbstractConnectionResources.validatePgConnectionStandardConformingStrings("PGSQL", mockPgConnection);
+  }
+
+
+  @Test
+  public void testValidatePgConnectionStandardConformingStringsFalse() {
+    MockPgConnection mockPgConnection = mock(MockPgConnection.class);
+    when(mockPgConnection.getStandardConformingStrings()).thenReturn(false);
+
+    IllegalStateException exception = assertThrows(IllegalStateException.class,
+      () -> AbstractConnectionResources.validatePgConnectionStandardConformingStrings("PGSQL", mockPgConnection));
+
+    assertThat(exception.getMessage(), Matchers.containsString("standard_conforming_strings=off"));
+  }
+
+
+  @Test
+  public void testValidatePgConnectionStandardConformingStringsMissingMethod() {
+    IllegalStateException exception = assertThrows(IllegalStateException.class,
+        () -> AbstractConnectionResources.validatePgConnectionStandardConformingStrings("PGSQL", mock(Connection.class)));
+
+    assertThat(exception.getMessage(), Matchers.containsString("no such method"));
+  }
+
+
+  private interface MockPgConnection extends Connection {
+    // see org.postgresql.core.BaseConnection.getStandardConformingStrings()
+    boolean getStandardConformingStrings();
+  }
+}
