b76749c4b3ff1d78417764d6ba3df0b3c0b58a77
jsimlo <jsimlo@users.noreply.github.com>
2024-11-14 13:11:58 +0000

    Adding javadocs to tests

--- morf-core/src/test/java/org/alfasoftware/morf/jdbc/TestAbstractConnectionResources.java
+++ morf-core/src/test/java/org/alfasoftware/morf/jdbc/TestAbstractConnectionResources.java
@@ -13,12 +13,18 @@ import org.mockito.Mockito;
 
 public class TestAbstractConnectionResources {
 
+  /**
+   * Test happy path for non-Postgres connections.
+   */
   @Test
   public void testValidatePgConnectionStandardConformingStringsOther() {
     AbstractConnectionResources.validatePgConnectionStandardConformingStrings("OTHER", mock(Connection.class));
   }
 
 
+  /**
+   * Test happy path for Postgres connections, where standard_conforming_strings is on, which is good.
+   */
   @Test
   public void testValidatePgConnectionStandardConformingStringsTrue() {
     MockPgConnection mockPgConnection = mock(MockPgConnection.class);
@@ -28,6 +34,9 @@ public class TestAbstractConnectionResources {
   }
 
 
+  /**
+   * Test unhappy path for Postgres connections, where standard_conforming_strings is off, and therefore wrong.
+   */
   @Test
   public void testValidatePgConnectionStandardConformingStringsFalse() {
     MockPgConnection mockPgConnection = mock(MockPgConnection.class);
@@ -40,15 +49,21 @@ public class TestAbstractConnectionResources {
   }
 
 
+  /**
+   * Test unhappy path for Postgres connections, where we cannot find the expected method.
+   */
   @Test
   public void testValidatePgConnectionStandardConformingStringsPrivateMethod() {
     IllegalStateException exception = assertThrows(IllegalStateException.class,
         () -> AbstractConnectionResources.validatePgConnectionStandardConformingStrings("PGSQL", mock(PrivatePgConnection.class, Mockito.CALLS_REAL_METHODS)));
 
-    assertThat(exception.getMessage(), Matchers.containsString("no such method"));
+    assertThat(exception.getMessage(), Matchers.containsString("invoking such method"));
   }
 
 
+  /**
+   * Test unhappy path for Postgres connections, where we cannot invoke the expected method.
+   */
   @Test
   public void testValidatePgConnectionStandardConformingStringsMissingMethod() {
     IllegalStateException exception = assertThrows(IllegalStateException.class,
@@ -58,12 +73,14 @@ public class TestAbstractConnectionResources {
   }
 
 
+  /** Helper interface for testing */
   private interface MockPgConnection extends Connection {
     // see org.postgresql.core.BaseConnection.getStandardConformingStrings()
     boolean getStandardConformingStrings();
   }
 
 
+  /** Helper class for testing */
   abstract class PrivatePgConnection implements Connection {
     // see org.postgresql.core.BaseConnection.getStandardConformingStrings()
     public boolean getStandardConformingStrings() throws java.lang.reflect.InvocationTargetException {
